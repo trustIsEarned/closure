1e25a36d2b34a55b4a312bd9c902d53827e9adb5
Allan Barch <apbarch@gmail.com>
2018-05-07 20:45:38 -0400

    closure web examples

---
+++ js/script.js
@@ -0,0 +1,77 @@
+function say667() {
+  // Local variable that ends up within closure
+  var num = 42;
+  var say = function() { console.log(num); }
+  num++;
+  return say;
+}
+var sayNumber = say667();
+sayNumber(); // logs 43
+
+
+
+var gLogNumber, gIncreaseNumber, gSetNumber;
+function setupSomeGlobals() {
+  // Local variable that ends up within closure
+  var num = 42;
+  // Store some references to functions as global variables
+  gLogNumber = function() { console.log(num); }
+  gIncreaseNumber = function() { num++; }
+  gSetNumber = function(x) { num = x; }
+}
+
+setupSomeGlobals();
+gIncreaseNumber();
+gLogNumber(); // 43
+gSetNumber(5);
+gLogNumber(); // 5
+
+var oldLog = gLogNumber;
+
+setupSomeGlobals();
+gLogNumber(); // 42
+
+oldLog() // 5
+
+let id = (function(){
+	
+
+	function increase(){
+		let idFirst = 1;
+		idFirst = idFirst+1;
+		console.log(idFirst);
+
+	}
+
+	return{
+		increase: increase
+	}
+
+})();
+
+
+let data = (function(){
+
+	let Person = function(name, age){
+		this.name = name;
+		this.age = age;
+	}
+
+	let key ="makeMoney";
+
+	function makePerson(name, age){
+		let person = new Person(name, age);
+		return person;
+	}	
+
+	return{
+		makePerson: makePerson,
+		password: key
+	}
+
+
+
+
+
+
+})();
