3a60bb9e2980c48c890533b5ab6cb17f1764d79c
Allan Barch <apbarch@gmail.com>
2018-05-07 21:02:05 -0400

    module pattern examples

--- js/script.js
+++ js/script.js
@@ -1,77 +1,127 @@
-function say667() {
-  // Local variable that ends up within closure
-  var num = 42;
-  var say = function() { console.log(num); }
-  num++;
-  return say;
-}
-var sayNumber = say667();
-sayNumber(); // logs 43
+// function say667() {
+//   // Local variable that ends up within closure
+//   var num = 42;
+//   var say = function() { console.log(num); }
+//   num++;
+//   return say;
+// }
+// var sayNumber = say667();
+// sayNumber(); // logs 43
 
 
 
-var gLogNumber, gIncreaseNumber, gSetNumber;
-function setupSomeGlobals() {
-  // Local variable that ends up within closure
-  var num = 42;
-  // Store some references to functions as global variables
-  gLogNumber = function() { console.log(num); }
-  gIncreaseNumber = function() { num++; }
-  gSetNumber = function(x) { num = x; }
-}
+// var gLogNumber, gIncreaseNumber, gSetNumber;
+// function setupSomeGlobals() {
+//   // Local variable that ends up within closure
+//   var num = 42;
+//   // Store some references to functions as global variables
+//   gLogNumber = function() { console.log(num); }
+//   gIncreaseNumber = function() { num++; }
+//   gSetNumber = function(x) { num = x; }
+// }
 
-setupSomeGlobals();
-gIncreaseNumber();
-gLogNumber(); // 43
-gSetNumber(5);
-gLogNumber(); // 5
+// setupSomeGlobals();
+// gIncreaseNumber();
+// gLogNumber(); // 43
+// gSetNumber(5);
+// gLogNumber(); // 5
 
-var oldLog = gLogNumber;
+// var oldLog = gLogNumber;
 
-setupSomeGlobals();
-gLogNumber(); // 42
+// setupSomeGlobals();
+// gLogNumber(); // 42
 
-oldLog() // 5
+// oldLog() // 5
 
-let id = (function(){
+// let id = (function(){
 	
 
-	function increase(){
-		let idFirst = 1;
-		idFirst = idFirst+1;
-		console.log(idFirst);
+// 	function increase(){
+// 		let idFirst = 1;
+// 		idFirst = idFirst+1;
+// 		console.log(idFirst);
 
-	}
+// 	}
 
-	return{
-		increase: increase
-	}
+// 	return{
+// 		increase: increase
+// 	}
 
-})();
+// })();
+
+
+// let data = (function(){
+
+// 	let Person = function(name, age){
+// 		this.name = name;
+// 		this.age = age;
+// 	}
+
+// 	let key ="makeMoney";
+
+// 	function makePerson(name, age){
+// 		let person = new Person(name, age);
+// 		return person;
+// 	}	
+
+// 	return{
+// 		makePerson: makePerson,
+// 		password: key
+// 	}
+
+// })();
+
+
 
 
 let data = (function(){
+	let name = "allan";
+	let age = 35;
 
-	let Person = function(name, age){
-		this.name = name;
-		this.age = age;
+	function changeAge(newAge){
+		age = newAge;
 	}
 
-	let key ="makeMoney";
-
-	function makePerson(name, age){
-		let person = new Person(name, age);
-		return person;
-	}	
-
-	return{
-		makePerson: makePerson,
-		password: key
+	function display(){
+		console.log(name + " "+age);
 	}
-
-
-
+	return {
+		changeAge:changeAge,
+		display: display
+	}
 
 
 
 })();
+
+let app = (function(data){
+	function load(){
+		data.changeAge(40);
+		data.display();
+	}
+
+	return{
+		load: load
+	}
+
+
+})(data);
+
+
+app.load();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
